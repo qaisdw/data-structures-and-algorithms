f2dbb2e21fe77a4213e22c53c39864a7e58c49ab
dwairi <qaisdw96@hotmail.com>
2023-05-16 02:09:20 +0000

    chal-06

--- linked-list/linked_list.py
+++ linked-list/linked_list.py
@@ -1,22 +1,22 @@
 class Node:
-    def __init__(self,value,next_node=None):
+    def __init__(self, value, next_node=None):
         self.value = value
-        self.next = next_node
+        self.next_node = next_node
 
 
 class Linkedlist:
-    def __init__(self,head=None):
+    def __init__(self, head=None):
         self.head = head
 
     def traverse(self):
         current_node = self.head
         while current_node:
             print(current_node.value)
-            current_node = current_node.next
+            current_node = current_node.next_node
 
     def insert(self, value):
         new_node = Node(value)
-        new_node.next = self.head
+        new_node.next_node = self.head
         self.head = new_node
 
     def includes(self, value):
@@ -25,16 +25,114 @@ class Linkedlist:
             if current_node.value == value:
                 print(current_node.value)
                 return True
-            current_node = current_node.next
+            current_node = current_node.next_node
         return False
 
     def to_string(self):
-        values = ""
+        values = "head -> "
         current_node = self.head
         while current_node:
             values += "{ " + str(current_node.value) + " } -> "
-            current_node = current_node.next
+            current_node = current_node.next_node
         values += "NULL"
         return values
 
+    def append(self, value):
+        newNode = Node(value)
 
+        if self.head is None:
+            self.head = newNode
+        else:
+            current_node = self.head
+            while current_node.next_node:
+                current_node = current_node.next_node
+            current_node.next_node = newNode
+
+    def addBefore(self, value, target_node):
+        newNode = Node(value)
+
+        if self.head is None:
+            print("Linked list is empty!")
+            return
+
+        if self.head.value == target_node:
+            newNode.next_node = self.head
+            self.head = newNode
+            return
+
+        current_node = self.head
+        while current_node.next_node:
+            if current_node.next_node.value == target_node:
+                break
+            current_node = current_node.next_node
+
+        if current_node.next_node is None:
+            print("Node is not found")
+        else:
+            newNode.next_node = current_node.next_node
+            current_node.next_node = newNode
+
+    def addAfter(self, value, target_node):
+        newNode = Node(value)
+
+        if self.head is None:
+            print("Linked list is empty!")
+            return
+
+        current_node = self.head
+        while current_node:
+            if current_node.value == target_node:
+                break
+            current_node = current_node.next_node
+
+        if current_node is None:
+            print("Node is not found")
+        else:
+            newNode.next_node = current_node.next_node
+            current_node.next_node = newNode
+
+    def delete(self, value):
+        if self.head is None:
+            print("Linked list is empty!")
+            return
+
+        if self.head.value == value:
+            self.head = self.head.next_node
+            return
+
+        current_node = self.head
+        prev_node = None
+
+        while current_node:
+            if current_node.value == value:
+                break
+            prev_node = current_node
+            current_node = current_node.next_node
+
+        if current_node is None:
+            print("Node is not found")
+            return
+        prev_node.next_node = current_node.next_node
+
+
+
+if __name__ == "__main__":
+    head = Node(1)
+    head.next_node = Node(3)
+    head.next_node.next_node = Node(2)
+    app = Linkedlist(head)
+
+    app.append(5)
+
+    app.addBefore(3, 5)
+
+    app.addAfter(3, 5)
+    app.delete(5)
+
+    current_node = app.head
+    while current_node:
+        print(current_node.value)
+        current_node = current_node.next_node
+
+    print("Initial List:")
+    print(app.to_string())
--- linked-list/tests/test_linked_list.py
+++ linked-list/tests/test_linked_list.py
@@ -4,25 +4,42 @@ from linked_list import Linkedlist
 def test_linked_list():
     # Test instantiation of an empty linked list
     ll = Linkedlist()
-    assert ll.head == None
+    assert ll.head is None
 
     # Test inserting a single node
-    ll.insert("a")
-    assert ll.head.value == "a"
+    ll.insert(1)
+    assert ll.head.value == 1
 
     # Test head property pointing to the first node
-    ll.insert("b")
-    ll.insert("c")
-    assert ll.head.value == "c"
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.head.value == 3
 
     # Test inserting multiple nodes
-    assert ll.to_string() == "{ c } -> { b } -> { a } -> NULL"
+    assert ll.to_string() == "head -> { 3 } -> { 2 } -> { 1 } -> NULL"
 
     # Test finding an existing value
-    assert ll.includes("b") == True
+    assert ll.includes(2) is True
 
     # Test finding a non-existing value
-    assert ll.includes("d") == False
+    assert ll.includes(4) is False
 
     # Test returning a collection of all values
-    assert ll.to_string() == "{ c } -> { b } -> { a } -> NULL"
+    assert ll.to_string() == "head -> { 3 } -> { 2 } -> { 1 } -> NULL"
+
+    # Test appending a node to the end
+    ll.append(4)
+    assert ll.to_string() == "head -> { 3 } -> { 2 } -> { 1 } -> { 4 } -> NULL"
+
+    # Test adding a node before a specific value
+    ll.addBefore(5, 1)
+    assert ll.to_string() == "head -> { 3 } -> { 2 } -> { 5 } -> { 1 } -> { 4 } -> NULL"
+
+    # Test adding a node after a specific value
+    ll.addAfter(6, 2)
+    assert ll.to_string() == "head -> { 3 } -> { 2 } -> { 6 } -> { 5 } -> { 1 } -> { 4 } -> NULL"
+
+    # Test deleting  a node in a specific value
+    ll.insert(2)
+    ll.delete(2)
+    assert ll.to_string() == "head -> { 3 } -> { 2 } -> { 6 } -> { 5 } -> { 1 } -> { 4 } -> NULL"
