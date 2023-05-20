7ed7568a9153499fdebf8f3f96cfdd4af0d2d9b7
dwairi <qaisdw96@hotmail.com>
2023-05-20 20:23:13 +0000

    chal-10

--- stack-and-queue/queuee.py
+++ stack-and-queue/queuee.py
@@ -1,14 +1,30 @@
 class Node:
     def __init__(self, value=None, next_node=None):
+        """
+        Initialize a Node object.
+
+        Args:
+            value: The value to be stored in the node.
+            next_node: The reference to the next node in the linked list.
+        """
         self.value = value
         self.next_node = next_node
 
 class Queue:
     def __init__(self):
+        """
+        Initialize an empty Queue object.
+        """
         self.front = None  # first node in the queue
         self.back = None   # last node in the queue
 
     def enqueue(self, value):
+        """
+        Adds a value to the back of the queue.
+
+        Args:
+            value: The value to be added to the queue.
+        """
         node = Node(value)
         if self.is_empty():
             self.front = node
@@ -17,6 +33,15 @@ class Queue:
         self.back = node
 
     def dequeue(self):
+        """
+        Removes and returns the value from the front of the queue.
+
+        Returns:
+            The value that was removed from the queue.
+
+        Raises:
+            ValueError: If the queue is empty.
+        """
         if self.is_empty():
             raise ValueError("Queue is empty. There is nothing to remove from the queue")
 
@@ -28,15 +53,36 @@ class Queue:
         return temp.value
 
     def peek(self):
+        """
+        Returns the value from the front of the queue without removing it.
+
+        Returns:
+            The value from the front of the queue.
+
+        Raises:
+            ValueError: If the queue is empty.
+        """
         if self.is_empty():
             raise ValueError("Queue is empty. There is nothing to peek in the queue")
 
         return self.front.value
 
     def is_empty(self):
+        """
+        Checks if the queue is empty.
+
+        Returns:
+            True if the queue is empty, False otherwise.
+        """
         return self.front is None
     
     def __str__(self):
+        """
+        Returns a string representation of the queue.
+
+        Returns:
+            A string representation of the queue.
+        """
         values = "head -> "
         current_node = self.front
         while current_node:
--- stack-and-queue/stack.py
+++ stack-and-queue/stack.py
@@ -1,13 +1,29 @@
 class Node:
     def __init__(self, value=None, next_node=None):
+        """
+        Initialize a Node object.
+
+        Args:
+            value: The value to be stored in the node.
+            next_node: The reference to the next node in the linked list.
+        """
         self.value = value
         self.next_node = next_node
 
 class Stack:
     def __init__(self):
+        """
+        Initialize an empty Stack object.
+        """
         self.top = None  # top node of the stack
 
     def push(self, value):
+        """
+        Pushes a value onto the top of the stack.
+
+        Args:
+            value: The value to be pushed onto the stack.
+        """
         node = Node(value)
         if self.is_empty():
             self.top = node
@@ -16,6 +32,15 @@ class Stack:
             self.top = node
 
     def pop(self):
+        """
+        Removes and returns the value from the top of the stack.
+
+        Returns:
+            The value that was popped from the stack.
+
+        Raises:
+            ValueError: If the stack is empty.
+        """
         if self.is_empty():
             raise ValueError("Stack is empty. There is nothing to pop from the stack.")
 
@@ -24,15 +49,36 @@ class Stack:
         return value
 
     def peek(self):
+        """
+        Returns the value from the top of the stack without removing it.
+
+        Returns:
+            The value from the top of the stack.
+
+        Raises:
+            ValueError: If the stack is empty.
+        """
         if self.is_empty():
             raise ValueError("Stack is empty. There is nothing to peek to in the queue.")
 
         return self.top.value
 
     def is_empty(self):
+        """
+        Checks if the stack is empty.
+
+        Returns:
+            True if the stack is empty, False otherwise.
+        """
         return self.top is None
     
     def __str__(self):
+        """
+        Returns a string representation of the stack.
+
+        Returns:
+            A string representation of the stack.
+        """
         values = "head -> "
         current_node = self.top
         while current_node:
